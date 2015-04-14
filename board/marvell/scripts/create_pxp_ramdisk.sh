84158332589bc4123fd3207f6b7c5e42acfc208a
Hanna Hawa <hannah@marvell.com>
2015-04-14 11:29:27 +0300

    buildroot: change base address of filesystem to 0x3000000
    Change-Id: I578bc5c341c193390fad4f0dbea10d524b1388f8
    Signed-off-by: Hanna Hawa <hannah@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/18242
    Reviewed-by: Shadi Ammouri <shadi@marvell.com>
    Tested-by: Shadi Ammouri <shadi@marvell.com>

--- board/marvell/scripts/create_pxp_ramdisk.sh
+++ board/marvell/scripts/create_pxp_ramdisk.sh
@@ -3,5 +3,5 @@
 export CROSS_COMPILE=output/host/usr/bin/aarch64-linux-gnu-
 ./board/marvell/scripts/mkimage -O linux -T ramdisk -A arm64 -C none -n "Ramdisk" -d $1/rootfs.ext2 $1/u_rootfs.ext2
 echo -n "bin2hex..."
-./board/marvell/scripts/bin2phex.pl -i $1/u_rootfs.ext2 -o $1/u_rootfs_ext2.hex -b 0x1080000
+./board/marvell/scripts/bin2phex.pl -i $1/u_rootfs.ext2 -o $1/u_rootfs_ext2.hex -b 0x3000000
 echo "Done."
