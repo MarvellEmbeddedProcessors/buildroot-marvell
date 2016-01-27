41dccdfb4c56b95fe1af88f9ee01861e656a82a9
Gustavo Zacarias <gustavo.zacarias@free-electrons.com>
2016-01-27 08:40:38 -0300

    dt: pull from upstream
    this commit syncs the dt package to mainline BR2 and fixes unavailable
    source site to a site maintained by Fedora
    
    Change-Id: I3ae5f29e3ec431f9d4cfcae6a3e4129746771be4
    Signed-off-by: Gustavo Zacarias <gustavo.zacarias@free-electrons.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/27522
    Reviewed-by: Lior Amsalem <alior@marvell.com>
    Tested-by: Star_Automation <star@marvell.com>
    Reviewed-by: Shadi Ammouri <shadi@marvell.com>

--- package/dt/dt.mk
+++ package/dt/dt.mk
@@ -1,30 +1,40 @@
-#############################################################
+################################################################################
 #
 # dt
 #
-#############################################################
-DT_VERSION:=15.14
-DT_SOURCE:=dt-source.tar.gz
-DT_SITE:=http://www.scsifaq.org/RMiller_Tools/ftp/dt
-DT_SUBDIR = dt.d-WIP
+################################################################################
+
+DT_VERSION = v18.32
+DT_SITE = http://pkgs.fedoraproject.org/repo/pkgs/dt/$(DT_SOURCE)/3054aeaaba047a1dbe90c2132a382ee2
+DT_SOURCE = dt-source-$(DT_VERSION).tar.gz
+DT_SUBDIR = dt.$(DT_VERSION)
+DT_LICENSE = ISC-like
+DT_LICENSE_FILES = $(DT_SUBDIR)/LICENSE
+
+DT_CFLAGS = \
+	-std=c99 \
+	-DMMAP \
+	-D__linux__ \
+	-D_GNU_SOURCE \
+	-D_FILE_OFFSET_BITS=64 \
+	-DTHREADS \
+	-DSCSI
+
+# uClibc doesn't provide POSIX AIO
+ifeq ($(BR2_TOOLCHAIN_USES_UCLIBC),)
+DT_CFLAGS += -DAIO
+endif
 
 define DT_BUILD_CMDS
-	$(MAKE) -C $(@D)/$(DT_SUBDIR) CC="$(TARGET_CC)" LD="$(TARGET_LD)" CFLAGS="$(TARGET_CFLAGS) -D__GNUC__ -DFIFO -DMMAP -D__linux__ -D_GNU_SOURCE" makedep
-	$(MAKE) -C $(@D)/$(DT_SUBDIR) CC="$(TARGET_CC)" LD="$(TARGET_LD)" CFLAGS="$(TARGET_CFLAGS) -DFIFO -DMMAP -DTTY -D__linux__ -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64"
-endef
-
-define DT_CONFIGURE_CMDS
-	cd $(@D)/$(DT_SUBDIR); ln -sf Makefile.linux Makefile
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/$(DT_SUBDIR) -f Makefile.linux \
+	$(TARGET_CONFIGURE_OPTS) \
+	CFLAGS="$(TARGET_CFLAGS) $(DT_CFLAGS)" \
+	OS=linux
 endef
 
 define DT_INSTALL_TARGET_CMDS
-	install -D $(@D)/${DT_SUBDIR}/dt $(TARGET_DIR)/usr/bin/dt
-endef
-
-define DT_CLEAN_CMDS
-	rm -f $(TARGET_DIR)/usr/bin/dt
-	-$(MAKE) -C $(@D)/${DT_SUBDIR} clean
+	$(INSTALL) -D -m 0755 $(@D)/$(DT_SUBDIR)/dt \
+		$(TARGET_DIR)/usr/bin/dt
 endef
 
 $(eval $(generic-package))
-$(eval $(package,dt))
